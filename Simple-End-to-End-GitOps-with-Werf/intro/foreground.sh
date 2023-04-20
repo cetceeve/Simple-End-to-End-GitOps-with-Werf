a227f9fcf2f563a915faeebfda46378ee9ea3595
Fabian Zeiher <fzeiher@gmail.com>
2023-04-20 17:47:35 +0200

    fix git repository init

--- Simple-End-to-End-GitOps-with-Werf/intro/foreground.sh
+++ Simple-End-to-End-GitOps-with-Werf/intro/foreground.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "Pre-building demo app image"
-docker build -t localhost:5000/demo-app:latest demo-app/
 echo "Installing local image registry..."
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
-echo DONE
+export LOCAL_REGISTRY="localhost:5000"
+echo "Setup git environment"
+git config --global user.email "demo@example.com"
+git config --global user.name "Killrcoder"
+echo $LOCAL_REGISTRY
+echo "All set! You are ready to go!"
---
+++ Simple-End-to-End-GitOps-with-Werf/run/text.md
@@ -0,0 +1,3 @@
+Lets go ahead and run werf for the first time
+
+`werf converge --repo localhost:5000/demo-app`{{exec}}
