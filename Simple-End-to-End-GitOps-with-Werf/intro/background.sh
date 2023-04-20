9d67bbb829eddc76e0b4f5b0aacc67d0a87c9abc
Fabian Zeiher <fzeiher@gmail.com>
2023-04-20 14:57:14 +0200

    move script to foreground

--- Simple-End-to-End-GitOps-with-Werf/intro/background.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 5000:5000 --restart=always --name registry registry:2
---
+++ Simple-End-to-End-GitOps-with-Werf/intro/foreground.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Installing local image registry..."
+docker run -d -p 5000:5000 --restart=always --name registry registry:2
+echo DONE
